6f2b2d57cccdd1a1a8acc177d7b828783764eb69
Elena Lucci <elena.lucci@gmail.com>
2025-07-16 12:02:41 +0200

    Inserito il Private

--- Esercizi/src/App.jsx
+++ Esercizi/src/App.jsx
@@ -3,32 +3,38 @@ import "./App.css";
 import InteractiveWelcome from "./components/InteractiveWelcome.jsx";
 import Login from "./components/Login.jsx";
 import Registrazione from "./components/Registrazione.jsx";
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Dashboard } from "./components/Dashboard.jsx";
-
+import Private from "./components/Private.jsx";
+import { useUser } from "./contexts/userContext.jsx";
 
 function App() {
-  const [users, setUsers] = useState(
-    JSON.parse(localStorage.getItem("users")) || [] //principio di contesto, dati accessibili a tutti i componenti: figli, nipoti.. Problema del props Drilling: un dato che passa da componente a figli
-  );
+  const {user} = useUser();
 
   return (
     <>
-    <BrowserRouter>
-      <nav>
-        <Link to="/" >Home</Link>
-        <Link to="/login" >Login</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<InteractiveWelcome />}/>
-        <Route path="/login" element={<Login database={users} />}/>
-        <Route path="/registrazione" element={<Registrazione database={users} />}/>
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-      
-      
-      
-    </BrowserRouter>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<InteractiveWelcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/registrazione"
+            element={<Registrazione />}
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <Private isAuth={user}>
+                <Dashboard />
+              </Private>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
